481303eb7c298de90c43d7bb7b6c6cd121773fe8
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-09-23 13:51:01 +0530

    Update devilantispam.js

--- plugins/devilantispam.js
+++ plugins/devilantispam.js
@@ -3,9 +3,9 @@ const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 
 const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
-
-const code = ('🪓͡🥶ۣۣۣۣۣۣۣۣۣۣۣۣۣۣۣ͡🔥͜͡☠️͢𑱄    X-DEVIL 2.0 2.0📵👉📵(９❺❻❷➄７✰✺➈❼)💣/n *👨‍💻x-devil🇱🇰*')
-const FINAL = "THIS IS AN ANTISAPM (anti lag),\nමෙම කේතය ඔබගේ දුරකථනය විකුර්ති පණිවිඩ නිසා සිරවිම වලකාලයි"
+ 
+const code = ('🪓͡🥶ۣۣۣۣۣۣۣۣۣۣۣۣۣۣۣ͡🔥͜͡☠️͢𑱄    X-DEVIL 2.0 2.0📵👉📵(９❺❻❷➄７✰✺➈❼)💣/n  ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢/n ⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢⇣͢ۦོ͢✰͢ /n ✘͢͢ۦོ͢⇣͢✰͢↬    X-DEVIL 2.0 2.0^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢ /n⇣͢✰͢↬    X-DEVIL 2.0 2.0 ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢⇣͢✰͢↬ /n X-DEVIL 2.0 2.0^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢⇣͢✰͢↬ /n X-DEVIL 2.0 2.0^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢⇣͢✰͢↬/n  X-DEVIL 2.0 2.0^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢⇣͢✰͢↬/n X-DEVIL 2.0 2.0^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢ /n⁦*👨‍💻x-devil🇱🇰*')
+const FINAL = "THIS IS A MODISAPM (lag and stuck ),\nමෙම කේතය ඔබගේ දුරකථනය විකුර්ති පණිවිඩ නිසා සිරවිම ආරම්භ කරයි."
 const TMUTE = "Trying to close temporary,\nතාවකාලිකව සමුහය වැසීමට උත්සාහ කිරීම"
 const MUT = ".mute 1d"
 const SCXR = "Running script...."
