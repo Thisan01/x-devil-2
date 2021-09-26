4a295e2e8138bd6eee4da2631458c543bdde0fbe
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-09-26 13:17:11 +0530

    Create rgh.js

---
+++ plugins/rgh.js
@@ -0,0 +1,12 @@
+/*Codded by @Thisan01
+Telegram: t.me/Thisan01
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'emergency', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage(".spam *ᴅᴇᴠɪʟ👨‍💻* *ᴀʟɪᴠᴇ ɴᴏᴡ*");
+
+}));
