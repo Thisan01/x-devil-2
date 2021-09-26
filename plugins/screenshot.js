99eb1cfe0be5f81a95a39331decf1a255409e537
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-09-26 15:19:01 +0530

    Create screenshot.js

---
+++ plugins/screenshot.js
@@ -0,0 +1,48 @@
+/* Codded by @Thisan01
+Telegram: t.me/Thisan01
+*/
+
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const request = require('request');
+const got = require("got");
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('webss');
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.LİNK);
+
+        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true&delay=400`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by *👨‍💻x-ᴅᴇᴠɪʟ🇱🇰*'})
+
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'ss ?(.*)', fromMe: false, desc: Lang.SS_DESC}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.LİNK);
+
+        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true&delay=400`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by *👨‍💻x-ᴅᴇᴠɪʟ🇱🇰*'})
+
+    }));
+    Asena.addCommand({pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.LİNK);
+
+        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true&delay=400`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by *👨‍💻x-ᴅᴇᴠɪʟ🇱🇰*'})
+
+    }));
+}
