c0592a12ae41a4e6c8feac68190bc09d56e01288
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-09-27 21:48:07 +0530

    Create version57.js

---
+++ plugins/version57.js
@@ -0,0 +1,12 @@
+/*Codded by Thisan01
+Telegram: t.me/Thisan01
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'version', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("*◈🤖DEVIL VERSION CHECKER🤖◈*\n\n*👨‍💻Devil ▻5.2.3*\n*Global Stable*");
+
+}));
